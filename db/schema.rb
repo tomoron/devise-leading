36b4b27becce030e558e7ecca308139e201a6184
tomoron <tomoron326@outlook.jp>
2022-09-07 14:18:37 +0900

    tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_023523) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_051122) do
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.integer "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_tweets_on_users_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_023523) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "tweets", "users", column: "users_id"
 end
